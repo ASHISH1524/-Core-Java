75cf83ed3251dcc9dc443e9ecbd9a9366aa7a1ef
Ashish Mahato <36353733+ASHISH1524@users.noreply.github.com>
2019-01-31 23:00:11 +0530

    Automated Billing system for Moter Garage.

---
+++ login/NewMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+/**
+ *
+ * @author Ashish Mahato
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
